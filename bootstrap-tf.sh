f643aa6ec977726767afa4c494d5902ec552e897
laupse <lgrondin@ingeniance.fr>
2023-08-07 15:37:04 +0200

    Add bootstrap-tf.sh

---
+++ bootstrap-tf.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+mkdir -p bin
+
+tfctl_version="v0.16.0-rc.2"
+tfctl_download_url="https://github.com/weaveworks/tf-controller/releases/download"
+
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./tfctl.tar.gz ${tfctl_download_url}/${tfctl_version}/tfctl_Linux_amd64.tar.gz
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./tfctl.tar.gz ${tfctl_download_url}/${tfctl_version}/tfctl_Linux_arm64.tar.gz
+tar -xzf ./tfctl.tar.gz tfctl
+mv ./tfctl ./bin/tfctl
+rm tfctl.tar.gz
+
+echo_green "Export helmrepository source tf-controller"
+./bin/flux create source helm tf-controller \
+  --url=https://weaveworks.github.io/tf-controller/ \
+  --verbose \
+  --interval=10m \
+  --export > ./clusters/kind/localstack-helm-repo.yaml
+
+echo_green "Export helmrelease tf-controller "
+./bin/flux create helmrelease localstack \
+  --chart=localstack \
+  --values="branchPlanner.enabled=true" \
+  --verbose \
+  --interval=30s \
+  --source=HelmRepository/tf-controller.flux-system \
+  --export > ./apps/localstack-helm-release.yaml
+
+echo_green "Export terraform ressources "
+./bin/tfctl create kube \
+  --verbose \
+  --interval=30s \
+  --source=GitRepository/flux-system.flux-system \
+  --path=".tf" \
+  --approve-plan="auto" \
+  --export > ./tf/flux/localstack-helm-release.yaml
+
+echo_green "Export kustomization localstack"
+./bin/flux create kustomization localstack \
+  --target-namespace=default \
+  --source=GitRepository/flux-system.flux-system \
+  --path="./tf/flux" \
+  --prune=true \
+  --wait=true \
+  --verbose \
+  --interval=30m \
+  --retry-interval=2m \
+  --health-check-timeout=3m \
+  --export > ./clusters/kind/localstack-kustomization.yaml
