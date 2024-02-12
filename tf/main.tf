66993a6fbad09a7576385e148f245abe91e45250
laupse <lgrondin@ingeniance.fr>
2024-02-12 13:22:10 +0100

    Fix cidr block

--- tf/main.tf
+++ tf/main.tf
@@ -34,20 +34,22 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
-
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "subnet_b" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server_a" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
